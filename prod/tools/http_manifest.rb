40d37f6827ed660fa46687e663722b8da59d1314
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2020-05-22 20:15:19 -0400

    use a either a direct implementation or use the default http client

--- prod/tools/http_manifest.rb
+++ prod/tools/http_manifest.rb
@@ -1,8 +1,37 @@
 require 'pathname'
 require_relative 'http_send'
+require "net/http"
+require "net/https"
+require 'multipart_body'
 require_relative '../../prod/def/ibc_aams.rb'
 include IBC::AAMS
 
+
+def send_http file, url
+
+        file_part = ""
+        File.open(file, 'rb') do |f|
+                file_part = Part.new :name => 'File', :body => f,
+                     :filename => file,
+                     :content_type => 'text/plain'
+        end
+
+        boundary = "---------------------------#{ rand(10000000000000000000) }"
+        body =  MultipartBody.new [file_part], boundary
+
+        uri = URI.parse url
+        http = Net::HTTP.new( uri.host, uri.port )
+        http.use_ssl = uri.scheme.eql? "https"
+        request = Net::HTTP::Post.new( uri.request_uri )
+
+        request.body = body.to_s
+        request.add_field( "Content-Type", "multipart/form-data; boundary=#{boundary}" )
+        response = http.request( request )
+
+        puts
+        puts "#{ response.inspect }"
+        puts "#{ response.body }"
+end
 
 e_code = 0
 begin
@@ -18,13 +47,21 @@ begin
 
         puts "AAMS Test File: #{ file }"
 
-        sender = HTTPSender.new true
-        sender.url = "https://api.pactrak.com/manifests/upload"
+# send the file using this function.
+
+        url = "https://api.pactrak.com/manifests/upload"
+
+        send_http file, url
+
+# or you can also use the IBC sender class and parse the JSON if you need
+=begin
+        sender = HTTPSender.new
+        sender.url = url
         sender.method = "POST"
         sender.execute file
 
         puts "#{ sender.response_json.response_message }"
-
+=end
 
 rescue Exception => exception
   STDERR.write "Exception: #{ exception }\n"
